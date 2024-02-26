123792db41fe1f62741a2e660c46493f4246ca34
Malte Renken <m.renken@mailbox.org>
2024-02-26 18:43:35 +0100

    Refactor johnson

--- src/flow_shop.rs
+++ src/flow_shop.rs
@@ -1,34 +1,64 @@
 use crate::jobs::{Time, Job};
 
+
 /// Optimally schedules jobs in a 2-machine flow shop to minimize makespan.
 /// This function uses Johnson's algorithm and takes O(n log n) time.
 /// See Johnson: "Optimal two- and three-stage production schedules with setup times included", 1954.
 ///
 /// # Arguments
-/// * processing_times: The processing time of each job on each of the two machines
+/// * processing_times: The processing time, where processing_times[i][j] is the time taken by machine i for job j
 ///
 /// # Returns
 /// A permutation of the jobs (i.e. of 0..n-1) such that scheduling the jobs in this order on both machines
 /// is an optimal solution ot the given F2||C_max instance.
-pub fn johnson(processing_times: &[[Time; 2]]) -> Vec<Job> {
-	let n = processing_times.len();
-	let group1 = (0..n).filter(|&j| processing_times[j][0] <= processing_times[j][1]);
-	let mut result : Vec<Job> = group1.collect();
-	result.sort_unstable_by_key(|&j| processing_times[j][0]);
-	let size1 = result.len();
-	let group2 = (0..n).filter(|&j| processing_times[j][0] > processing_times[j][1]);
-	result.extend(group2);
-	result[size1..].sort_unstable_by_key(|&j| -processing_times[j][1]);
+pub fn johnson(processing_times: &[Vec<Time>]) -> Vec<Job> {
+	assert!(processing_times.len() == 2, "Instance must have exactly 2 machines");
+	let n = processing_times[0].len();
+	let mut result : Vec<Job> = (0..n).collect();
+	let num1 = partition_in_place(&mut result, 
+		|&j| processing_times[0][j] <= processing_times[1][j]
+	);
+	result[..num1].sort_unstable_by_key( |&j|  processing_times[0][j] );
+	result[num1..].sort_unstable_by_key( |&j| -processing_times[1][j] );
 	result
 }
+
+
+/// Reorders a vector in place according to a predicate function,
+/// such that all items satisfying the predicate come before any other item.
+///
+/// # Returns
+/// The number of items that satisfy the predicate
+fn partition_in_place<T, F>(container: &mut Vec<T>, mut predicate: F) -> usize
+where
+	F: FnMut(&T) -> bool,
+{
+	let mut i1 = 0;
+	let mut i2 = container.len() - 1;
+	loop {
+		while i1 < container.len() - 1 && predicate(&container[i1]) {
+			i1 += 1;
+		}
+		while i2 > 0 && !predicate(&container[i2]) {
+			i2 -= 1;
+		}
+		if i1 < i2 {
+			container.swap(i1, i2);
+		}
+		else {
+			return i1;
+		}
+	}
+}
+
 
 
 #[cfg(test)]
 mod tests {
 	use super::*;
 
-	fn example_1() -> Vec<[Time; 2]> {
-		vec![[3, 4], [2, 1], [1, 5]]
+	fn example_1() -> Vec<Vec<Time>> {
+		vec![vec![3, 2, 1], vec![4, 1, 5]]
 	}
 
 	#[test]
@@ -36,5 +66,15 @@ mod tests {
 		let result = johnson(&example_1());
 		let expected = vec![2, 0, 1];
 		assert_eq!(result, expected);
+	}
+
+	#[test]
+	fn test_partition_in_place() {
+		let mut container = vec![3, 4, 7, 1, 0, 2, 0, 4];
+		let is_even : fn(&i32) -> _ = |x| x % 2 == 0;
+		let k = partition_in_place(&mut container, is_even);
+		container[..k].sort_unstable();
+		container[k..].sort_unstable();
+		assert_eq!(container, vec![0, 0, 2, 4, 4, 1, 3, 7]);
 	}
 }
